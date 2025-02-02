6ac5b828ae03e0e806d4e820a198dbd9746639c3
Austin Davis <austindavis@gmail.com>
2025-02-02 13:20:33 -0500

    Add square names to chess vocabulary in uci_tokenizers.py to eliminate chess package dependency

--- dev/data/uci_tokenizers.py
+++ dev/data/uci_tokenizers.py
@@ -1,5 +1,71 @@
-import chess
 import tiktoken
+
+SQUARE_NAMES = [
+    "a1",
+    "b1",
+    "c1",
+    "d1",
+    "e1",
+    "f1",
+    "g1",
+    "h1",
+    "a2",
+    "b2",
+    "c2",
+    "d2",
+    "e2",
+    "f2",
+    "g2",
+    "h2",
+    "a3",
+    "b3",
+    "c3",
+    "d3",
+    "e3",
+    "f3",
+    "g3",
+    "h3",
+    "a4",
+    "b4",
+    "c4",
+    "d4",
+    "e4",
+    "f4",
+    "g4",
+    "h4",
+    "a5",
+    "b5",
+    "c5",
+    "d5",
+    "e5",
+    "f5",
+    "g5",
+    "h5",
+    "a6",
+    "b6",
+    "c6",
+    "d6",
+    "e6",
+    "f6",
+    "g6",
+    "h6",
+    "a7",
+    "b7",
+    "c7",
+    "d7",
+    "e7",
+    "f7",
+    "g7",
+    "h7",
+    "a8",
+    "b8",
+    "c8",
+    "d8",
+    "e8",
+    "f8",
+    "g8",
+    "h8",
+]
 
 
 def chessGptTokenizer() -> tiktoken.Encoding:
@@ -16,16 +82,15 @@ def chessGptTokenizer() -> tiktoken.Encoding:
     """
     special_tokens = ["<|pad|>", "<|startoftext|>", "<|endoftext|>", "<|unknown|>"]
     unused_tokens = [f"<|unused{i:04d}" for i in range(8120)]
-    chess_vocab = special_tokens + chess.SQUARE_NAMES + list("QBRN") + unused_tokens
-    mergeable_ranks = {k.encode():v for (v,k) in enumerate(chess_vocab)}
-    chess_pat_str = r'[a-h][1-8]|[QBRN]'
+    chess_vocab = special_tokens + SQUARE_NAMES + list("QBRN") + unused_tokens
+    mergeable_ranks = {k.encode(): v for (v, k) in enumerate(chess_vocab)}
+    chess_pat_str = r"[a-h][1-8]|[QBRN]"
 
     enc = tiktoken.Encoding(
         name="chess_enc",
-        pat_str=chess_pat_str, # or \d|\s
+        pat_str=chess_pat_str,  # or \d|\s
         mergeable_ranks=mergeable_ranks,
-        special_tokens={k:v for (v,k) in enumerate(special_tokens)},
+        special_tokens={k: v for (v, k) in enumerate(special_tokens)},
     )
 
     return enc
-
